28152c946868d2e457fb99f3bd668848a343f700
Hugo Ramon <hramonp-dev@outlook.com>
2025-05-19 15:17:17 -0300

    Created header component, updated routes and dynamic page title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { EditorComponent } from './pages/editor/editor.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: MainLayoutComponent
-  }
+    component: MainLayoutComponent,
+    children: [
+      { path: 'editor', component: EditorComponent, data: { title: 'Editor' } },
+      { path: 'pricing', component: PricingComponent, data: { title: 'Pricing' } },
+    ],
+  },
+  { path: '**', component: PageNotFoundComponent, data: { title: 'Page Not Found' } },
 ];
